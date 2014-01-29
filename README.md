75a2c82033275bb96e1826b76ac9d155b8a616d1
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2014-01-30 00:01:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# cq-unix-toolkit cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Jakub Wadolowski (<jakub.wadolowski@cognifide.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cq-unix-toolkit'
+maintainer       'Jakub Wadolowski'
+maintainer_email 'jakub.wadolowski@cognifide.com'
+license          'Apache 2.0'
+description      'Installs/Configures cq-unix-toolkit'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
