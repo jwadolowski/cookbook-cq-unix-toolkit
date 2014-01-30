36cce3379543acdaa32f32e6c57bfe69dcbdb37f
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2014-01-30 18:36:31 +0100

    Attribute file has been added to the cookbook

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+# Repository settings
+default[:cq_unix_toolkit][:repository][:url] = "https://github.com/Cognifide/CQ-Unix-Toolkit.git"
+default[:cq_unix_toolkit][:repository][:revison] = "v1.0.0"
+
+# General settings
+default[:cq_unix_toolkit][:checkout_dir] = "/opt/scripts/CQ-Unix-Toolkit"
+default[:cq_unix_toolkit][:install?] = false
