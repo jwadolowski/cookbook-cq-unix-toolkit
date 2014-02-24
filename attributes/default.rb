15adc22bda8497578d8f7de128fd30c9d7cc9bec
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2014-02-24 12:57:55 +0100

    Rubocop fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,10 @@
-# Repository settings
-default[:cq_unix_toolkit][:repository][:url] = "https://github.com/Cognifide/CQ-Unix-Toolkit.git"
-default[:cq_unix_toolkit][:repository][:revison] = "v1.0.0"
+# encoding: UTF-8
+
+# Repository setting:s
+default[:cq_unix_toolkit][:repository][:url] =
+  'https://github.com/Cognifide/CQ-Unix-Toolkit.git'
+default[:cq_unix_toolkit][:repository][:revison] = 'v1.0.0'
 
 # General settings
-default[:cq_unix_toolkit][:checkout_dir] = "/opt/scripts/CQ-Unix-Toolkit"
+default[:cq_unix_toolkit][:checkout_dir] = '/opt/scripts/CQ-Unix-Toolkit'
 default[:cq_unix_toolkit][:install?] = false
