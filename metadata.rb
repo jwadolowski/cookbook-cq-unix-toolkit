f357b464a72b95f01953c8b09268b6ab0073766c
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2018-04-10 12:19:33 +0200

    Foodcritic/metadata fixes

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,20 @@
 name             'cq-unix-toolkit'
 maintainer       'Jakub Wadolowski'
 maintainer_email 'jakub.wadolowski@cognifide.com'
-license          'Apache 2.0'
-description      'Installs/Configures cq-unix-toolkit'
-long_description 'Installs/Configures cq-unix-toolkit'
+license          'Apache-2.0'
+description      'Installs and configures CQ UNIX Toolkit'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.1'
 
 depends          'git'
 
-supports         'debian'
-supports         'centos'
-supports         'redhat'
+source_url       'https://github.com/jwadolowski/cookbook-cq'
+issues_url       'https://github.com/jwadolowski/cookbook-cq/issues'
+
+chef_version     '~> 12'
+
+supports         'centos', '~> 6.0'
+supports         'centos', '~> 7.0'
+
+supports         'redhat', '~> 6.0'
+supports         'redhat', '~> 7.0'
